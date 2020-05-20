944354ac45023640b36d4ba9b9f047d578efb7d0
kirilraykov <kirilraykov@gmail.com>
2020-05-21 00:32:55 +0300

    Cleanup simulations

---
+++ src/test/scala/simulations/GetCitiesSimulation.scala
@@ -0,0 +1,32 @@
+package simulations
+
+import scala.concurrent.duration._
+import io.gatling.core.Predef._
+import io.gatling.core.scenario.Simulation
+import io.gatling.http.Predef._
+import src.test.scala.UserService.getAccessModules
+
+class GetCitiesSimulation extends Simulation {
+
+  val httpConf = http.baseUrl("http://localhost:8093")
+    .header("Accept", "application/json")
+
+  val scn = scenario("Get all cities - Ramp up users scenario")
+    .exec(getAccessModules)
+
+  /**
+   * Injection Design:
+   * 1) Start with wait - simulate loading or pause
+   * 2) Inject 10 users at once
+   * 3) start to ramp up users to 250 for 2 minutes
+   * 4) Do nothing for the next 40 seconds and inject 5 new users at once
+   */
+  setUp(
+    scn.inject(
+      nothingFor(3 seconds),
+      atOnceUsers(5),
+      constantUsersPerSec(20) during(20 minutes),
+      ).protocols(httpConf.inferHtmlResources())
+    ).maxDuration(4 minutes)
+
+}
---
+++ src/test/scala/simulations/SignupNewClientSimulation.scala
@@ -0,0 +1,50 @@
+package simulations
+
+import io.gatling.core.Predef._
+import io.gatling.core.scenario.Simulation
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
+import src.test.scala.UserService.{getAccessModules, signupNewUser, getCities, createClient, getClient}
+import src.test.scala.SportService.getSports
+
+class SignupNewClientSimulation extends Simulation {
+
+  private def getProperty(propertyName: String, defaultValue: String) = {
+    Option(System.getenv(propertyName))
+      .orElse(Option(System.getProperty(propertyName)))
+      .getOrElse(defaultValue)
+  }
+
+  def userCount: Int = getProperty("rampUsers", "200").toInt
+  def rampDuration: Int = getProperty("rampDuration", "2").toInt
+  def testDuration: Int = getProperty("totalDuration", "4").toInt
+
+  before {
+    println(s"Ramping up users to totoal: $userCount users")
+    println(s"Ramping users over: $rampDuration minutes")
+    println(s"Total test duration: $testDuration minutes")
+  }
+
+  val httpConf = http.baseUrl("http://localhost:8093")
+    .header("Accept", "application/json")
+//    .proxy(Proxy("localhost", 8888))
+
+  val scn = scenario("Signup new Client Scenario")
+    .exec(getAccessModules)
+    .exec(signupNewUser)
+    .exec(getCities)
+    .exec(getSports)
+    .exec(createClient)
+    .exec(getClient)
+
+  setUp(
+    scn.inject(
+      nothingFor(3 seconds),
+      atOnceUsers(10),
+      rampUsers(userCount) during (rampDuration minutes),
+      nothingFor(40 seconds),
+      atOnceUsers(5),
+      ).protocols(httpConf.inferHtmlResources())
+    ).maxDuration(testDuration minutes)
+
+}
